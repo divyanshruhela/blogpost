1e3db85e52036fbe52826e05d36df06288b77a2a
Divyansh Ruhela <divyansh.ruhela@example.com>
2022-01-23 00:10:22 +0530

    Latest changes

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import IndexScreen from "./assets/Screens/IndexScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
   },
-});
+  {
+    initialRouteName: "Index",
+    defaultnavigationOptions: {
+      title: "Blogs",
+    },
+  }
+);
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
---
+++ assets/Screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet } from 'react-native';
+
+
+const IndexScreen = ()=>{
+
+    return <View>
+        <Text>Helooooo</Text>
+    </View>
+}
+
+
+export default IndexScreen;
